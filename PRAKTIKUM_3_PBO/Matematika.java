a2ee0b0c9c3733ae09a6109ae966ea47c0cb0a84
restudev <restulestarii.m@gmail.com>
2024-03-20 19:05:40 +0700

    update

--- PRAKTIKUM_3_PBO/Matematika.java
+++ PRAKTIKUM_3_PBO/Matematika.java
@@ -1,14 +1,37 @@
 public class Matematika {
-    float pertambahan(float a, float b){
-        return a + b;
+    // float pertambahan(float a, float b){
+    //     return a + b;
+    // }
+    // float pengurangan(float a, float b){
+    //     return a - b;
+    // } 
+    // float perkalian(float a, float b){
+    //     return a * b;
+    // } 
+    // float pembagian(float a, float b){
+    //     return a / b;
+    // }     
+
+    float tambah = 0;
+    float kurang = 0;
+    float kali = 0;
+    float bagi = 0;
+    float a, b;
+
+    void pertambahan (float a, float b) {
+        tambah = a + b;
+        System.out.println("\n Hasil Pertambahan: " + a + " + " + b + " = " + tambah);
     }
-    float pengurangan(float a, float b){
-        return a - b;
-    } 
-    float perkalian(float a, float b){
-        return a * b;
-    } 
-    float pembagian(float a, float b){
-        return a / b;
-    }     
+    void pengurangan (float a, float b) {
+        kurang = a - b;
+        System.out.println("\n Hasil Pengurangan: " + a + " - " + b + " = " + kurang);
+    }
+    void perkalian (float a, float b) {
+        kali = a * b;
+        System.out.println("\n Hasil Perkalian: " + a + " x " + b + " = " + kali);
+    }
+    void pembagian (float a, float b) {
+        bagi = a / b;
+        System.out.println("\n Hasil Pembagian: " + a + " : " + b + " = " + bagi);
+    }
 }
--- PRAKTIKUM_3_PBO/MatematikaDemo.java
+++ PRAKTIKUM_3_PBO/MatematikaDemo.java
@@ -1,21 +1,26 @@
 public class MatematikaDemo {
     public static void main(String[] args) {
-        Matematika matematika = new Matematika();
+        // Matematika matematika = new Matematika();
 
-        // Eksekusi method dan menampilkan hasilnya
-        float a = 20, b = 20;
-        System.out.println("Hasil Pertambahan: " + a + " + " + b + " = " + matematika.pertambahan(a, b));
+        // float a = 20, b = 20;
+        // System.out.println("Hasil Pertambahan: " + a + " + " + b + " = " + matematika.pertambahan(a, b));
 
-        a = 10;
-        b = 5;
-        System.out.println("Hasil Pengurangan: " + a + " - " + b + " = " + matematika.pengurangan(a, b));
+        // a = 10;
+        // b = 5;
+        // System.out.println("Hasil Pengurangan: " + a + " - " + b + " = " + matematika.pengurangan(a, b));
 
-        a = 10;
-        b = 20;
-        System.out.println("Hasil Perkalian: " + a + " x " + b + " = " + matematika.perkalian(a, b));
+        // a = 10;
+        // b = 20;
+        // System.out.println("Hasil Perkalian: " + a + " x " + b + " = " + matematika.perkalian(a, b));
 
-        a = 20;
-        b = 2;
-        System.out.println("Hasil Pembagian: " + a + " / " + b + " = " + matematika.pembagian(a, b));
+        // a = 20;
+        // b = 2;
+        // System.out.println("Hasil Pembagian: " + a + " / " + b + " = " + matematika.pembagian(a, b));
+
+        Matematika hitung = new Matematika();
+        hitung.pertambahan(20, 20);
+        hitung.pengurangan(10, 5);
+        hitung.perkalian(10, 20);
+        hitung.pembagian(20, 2);
     }
 }
