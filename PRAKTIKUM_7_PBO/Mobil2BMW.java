d16ca09888f3f65367b7d418292a4579348130e7
restudev <restulestarii.m@gmail.com>
2024-04-30 13:17:47 +0700

    done ✅

--- PRAKTIKUM_7_PBO/Mobil2BMW.java
+++
@@ -1,8 +0,0 @@
-public class Mobil2BMW extends Mobil2 {
-    public void nontonTV() {
-        System.out.println("TV dihidupkan");
-        System.out.println("TV mencari channel");
-        System.out.println("TV menampilkan gambar");
-    }
-
-}
