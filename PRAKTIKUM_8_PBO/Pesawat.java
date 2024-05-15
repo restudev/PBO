a9e1e57572724467cd73a053f4b74fb3d4abdf8c
restudev <restulestarii.m@gmail.com>
2024-05-15 09:59:13 +0700

    Add Pesawat class

---
+++ PRAKTIKUM_8_PBO/Pesawat.java
@@ -0,0 +1,15 @@
+public class Pesawat {
+    int sayap, ekor;
+    
+    Pesawat(){
+        System.out.println("object Pesawat dibuat............");
+    }
+
+    void terbang(){
+        System.out.println("Terbang..........");
+    }
+
+    void mendarat(){
+        System.out.println("Mendarat..........");
+    }
+}
