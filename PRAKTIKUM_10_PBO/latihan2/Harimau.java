f1136e262475374609cec12291a0bf8b2d4f6577
restudev <restulestarii.m@gmail.com>
2024-05-18 15:21:58 +0700

    add code praktikum 10

---
+++ PRAKTIKUM_10_PBO/latihan2/Harimau.java
@@ -0,0 +1,10 @@
+package latihan2;
+
+class Harimau extends Binatang {
+    void makan() {
+        System.out.println("Harimau Makan..........");
+    }
+    void tidur() {
+        System.out.println("Harimau tidur..........");
+    }
+}
