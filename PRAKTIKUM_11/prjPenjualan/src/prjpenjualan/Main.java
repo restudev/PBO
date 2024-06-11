7a47ed0586be19a5836e016f666d261b53461926
Restu <122165425+restudev@users.noreply.github.com>
2024-06-11 10:13:57 +0700

    Add files via upload

---
+++ PRAKTIKUM_11/prjPenjualan/src/prjpenjualan/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prjpenjualan;
+import java.sql.*;
+
+/**
+ *
+ * @author D2A
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        KoneksiMysql kon = new KoneksiMysql ("penjualan");
+        Connection c = kon.getConnection();
+        System.out.println("c : " + c);
+    }
+    
+}
