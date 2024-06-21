d0937374dbfa7f5251a14a1b01e0fbeb8033a537
restudev <restulestarii.m@gmail.com>
2024-06-21 19:57:38 +0700

    done✅

---
+++ PRAKTIKUM_11_PBO/penjualan.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 21, 2024 at 02:21 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `kd_brg` char(6) NOT NULL,
+  `nm_brg` varchar(30) NOT NULL,
+  `satuan` varchar(10) DEFAULT NULL,
+  `harga_jual` double DEFAULT NULL,
+  `harga_beli` double DEFAULT NULL,
+  `stok` int(4) DEFAULT NULL,
+  `stok_min` int(4) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `konsumen`
+--
+
+CREATE TABLE `konsumen` (
+  `kd_kons` varchar(6) NOT NULL,
+  `nm_kons` varchar(30) DEFAULT NULL,
+  `alm_kons` varchar(50) DEFAULT NULL,
+  `kota_kons` varchar(20) DEFAULT NULL,
+  `kd_pos` varchar(5) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`kd_brg`);
+
+--
+-- Indexes for table `konsumen`
+--
+ALTER TABLE `konsumen`
+  ADD PRIMARY KEY (`kd_kons`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ PRAKTIKUM_11_PBO/prjpenjualan/src/main/java/com/restu/prjpenjualan/KoneksiMySql.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.restu.prjpenjualan;
+
+/**
+ *
+ * @author Restu
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author rdannyoka
+ */
+public class KoneksiMySql {
+    String url, usr, pwd, dbn;
+    public KoneksiMySql (String dbn) {
+        this.url = "jdbc:mysql://localhost/" + dbn;
+        this.usr = "root"; // Sesuaikan dengan username db anda
+        this.pwd = "134679"; // Sesuaikan dengan password db anda
+    }
+    public KoneksiMySql (String host, String user, String pass,String dbn) {
+        this.url = "jdbc:mysql://" + host + "/" + dbn;
+        this.usr = user;
+        this.pwd = pass;
+    }
+    
+    public Connection getConnection() {
+    Connection con = null;
+    try {
+//        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con = DriverManager.getConnection(this.url, this.usr, this.pwd);
+        System.out.println("Koneksi Berhasil");
+    } catch (ClassNotFoundException e) {
+        System.out.println ("Error #1 : " + e.getMessage());
+        System.exit(0);
+    } catch (SQLException e) {
+        System.out.println ("Error #2 : " + e.getMessage());
+        System.exit(0);
+    }
+        return con;
+    }
+    
+    public static void main (String args[]) {
+        KoneksiMySql kon = new KoneksiMySql ("penjualan"); //Sesuaikan dengan nama database anda
+        Connection c = kon.getConnection();
+    }
+    
+}
