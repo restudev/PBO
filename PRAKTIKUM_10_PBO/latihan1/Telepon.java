b4d4f0914af31134da107458dd59546375776665
restudev <restulestarii.m@gmail.com>
2024-06-26 08:38:50 +0700

    update

---
+++ PRAKTIKUM_10_PBO/latihan1/Telepon.java
@@ -0,0 +1,8 @@
+package latihan1;
+
+abstract class Telepon {
+    protected long nomer;
+    public void telpon(){
+        System.out.println("Sedang Menelepon");
+    }
+}
---
+++ PRAKTIKUM_10_PBO/latihan2/Binatang.java
@@ -0,0 +1,9 @@
+package latihan2;
+
+abstract class Binatang {
+    abstract void makan();
+    abstract void tidur();
+    void mati() {
+        System.out.println("Mati..........");
+    }
+}
