6311965c9d1b39e4534ec788916895745e24ee0e
restudev <restulestarii.m@gmail.com>
2024-06-26 09:01:52 +0700

    update

--- PRAKTIKUM_11_12_PBO/prjpenjualan/src/main/java/com/restu/prjpenjualan/KoneksiMySql.java
+++ PRAKTIKUM_11_12_PBO/prjpenjualan/src/main/java/com/restu/prjpenjualan/KoneksiMySql.java
@@ -8,47 +8,38 @@ package com.restu.prjpenjualan;
  *
  * @author Restu
  */
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
-/**
- *
- * @author Restu
- */
 public class KoneksiMySql {
+
     String url, usr, pwd, dbn;
-    public KoneksiMySql (String dbn) {
+
+    public KoneksiMySql(String dbn) {
         this.url = "jdbc:mysql://localhost/" + dbn;
-        this.usr = "root"; // Sesuaikan dengan username db
-        this.pwd = ""; // Sesuaikan dengan password db
+        this.usr = "root";
+        this.pwd = "";
     }
-    public KoneksiMySql (String host, String user, String pass,String dbn) {
+
+    public KoneksiMySql(String host, String user, String pass,
+            String dbn) {
         this.url = "jdbc:mysql://" + host + "/" + dbn;
         this.usr = user;
         this.pwd = pass;
     }
-    
+
     public Connection getConnection() {
-    Connection con = null;
-    try {
-//        Class.forName("com.mysql.jdbc.Driver");
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        con = DriverManager.getConnection(this.url, this.usr, this.pwd);
-        System.out.println("Koneksi Berhasil");
-    } catch (ClassNotFoundException e) {
-        System.out.println ("Error #1 : " + e.getMessage());
-        System.exit(0);
-    } catch (SQLException e) {
-        System.out.println ("Error #2 : " + e.getMessage());
-        System.exit(0);
-    }
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(this.url, this.usr, this.pwd);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error #1 : " + e.getMessage());
+            System.exit(0);
+        } catch (SQLException e) {
+            System.out.println("Error #2 : " + e.getMessage());
+            System.exit(0);
+        }
         return con;
     }
-    
-    public static void main (String args[]) {
-        KoneksiMySql kon = new KoneksiMySql ("penjualan"); //Sesuaikan dengan nama database
-        Connection c = kon.getConnection();
-    }
-    
+
 }
