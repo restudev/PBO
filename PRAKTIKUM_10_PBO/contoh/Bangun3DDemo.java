b1ac36099adb3ed56aa4ed74902ae88998e91bc2
restudev <restulestarii.m@gmail.com>
2024-06-26 22:27:20 +0700

    done ✅

--- PRAKTIKUM_10_PBO/contoh/Bangun3DDemo.java
+++
@@ -1,9 +0,0 @@
-package contoh;
-
-class Bangun3DDemo {
-    public static void main(String[] args) {
-        BujurSangkar bs = new BujurSangkar(5);
-        bs.cetakKeliling();
-        bs.cetakLuas();
-    }
-}
