3312122253433c8e310f62d34f66505640af01cb
restudev <restulestarii.m@gmail.com>
2024-07-05 15:35:02 +0700

    update

--- PRAKTIKUM_10_PBO/latihan1/Kamera.java
+++
@@ -1,6 +0,0 @@
-package latihan1;
-
-public interface Kamera {
-    public void setPixel(float pixel);
-    public void ambilGambar();
-}
--- PRAKTIKUM_10_PBO/latihan1/Radio.java
+++
@@ -1,5 +0,0 @@
-package latihan1;
-
-public interface Radio {
-    public void setGelombang(String gel);
-}
